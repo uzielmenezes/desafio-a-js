ca255ef24e9df34bf04edcc6ace58246c57993f4
Uziel Menezes <uzielcivil@gmail.com>
2022-05-17 07:42:02 -0300

    Giving each 'Conta' its own name by extending Conta.js to our other modules and calling a super constructor

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,13 +1,10 @@
 import {Cliente} from "./Cliente.js"
+import {Conta} from "./Conta.js"
 
-export class ContaCorrente { // exportando o módulo da classe para o index.js
-
+export class ContaCorrente extends Conta { // exportando o módulo da classe para o index.js
 	static numeroDeContas = 0;
-
-	constructor(agencia, cliente ) {  // construtor adicionado para inicializar os atributos agencia e cliente
-		this.agencia = agencia;
-		this.cliente = cliente;
-		this._saldo = 0; // tornando o atributo privado com underline // verificar proposta com # https://github.com/tc39/proposal-class-fields#private-fields
+	constructor(cliente, agencia) {  
+		super(0, cliente, agencia);
 		ContaCorrente.numeroDeContas += 1;
 	}
 }
--- index.js
+++ index.js
@@ -8,11 +8,11 @@ import {Conta} from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309); // objetos criados a partir do molde Cliente com constructor definido
 
-const contaCorrente1 = new Conta(0, cliente1, 1001); // objetos/instâncias criados a partir do molde ContaCorrente com constructor definido;
+const contaCorrente1 = new ContaCorrente(cliente1, 1001); // objetos/instâncias criados a partir do molde ContaCorrente com constructor definido;
 contaCorrente1.depositar(500);
 contaCorrente1.sacar(100);
 
-const contaPoupanca1 = new Conta(50, cliente1, 1001);
+const contaPoupanca1 = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca1);
 console.log(contaCorrente1);
