b2717de24a0f7174b0721dfbb45f17357fac6469
Uziel Menezes <uzielcivil@gmail.com>
2022-05-17 06:25:59 -0300

    Added a new js file for ContaPoupanca and small changes about constructors to optimize our codes

--- Cliente.js
+++ Cliente.js
@@ -1,15 +1,13 @@
 export class Cliente {  // exportando o módulo da classe para o index.js
 						// criando a classe para o ''molde'' Cliente
-	nome;
-	_cpf;            // transformando o atributo cpf privado e conectado
-					// diretamente com o nome
-	get cpf() {				// tornando o cpf somente buscável e não modificável
-		return this._cpf;
-	}
 
 	constructor(nome, cpf) { // construtor adicionado para inicializar os atributos nome e cpf
 		this.nome = nome;
 		this._cpf = cpf;
+	}
+	
+	get cpf() {				// tornando o cpf somente buscável e não modificável
+		return this._cpf;
 	}
 
 }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,15 @@
 import {Cliente} from "./Cliente.js"
 
-export class ContaCorrente { // exportando o módulo da classe para o index.js	
+export class ContaCorrente { // exportando o módulo da classe para o index.js
+
 	static numeroDeContas = 0;
-	agencia;				// criando a classe para o ''molde'' ContaCorrente
-	_cliente;
+
+	constructor(agencia, cliente ) {  // construtor adicionado para inicializar os atributos agencia e cliente
+		this.agencia = agencia;
+		this.cliente = cliente;
+		this._saldo = 0; // tornando o atributo privado com underline // verificar proposta com # https://github.com/tc39/proposal-class-fields#private-fields
+		ContaCorrente.numeroDeContas += 1;
+	}
 
 	set cliente(novoValor) {						// criando condições para modificar o cliente e tornando buscável não somente cliente como o saldo
 		if(novoValor instanceof Cliente) {
@@ -15,16 +21,8 @@ export class ContaCorrente { // exportando o módulo da classe para o index.js
 		return this._cliente;
 	}
 
-	_saldo = 0; // tornando o atributo privado com underline // verificar proposta com # https://github.com/tc39/proposal-class-fields#private-fields
-
 	get saldo() {
 		return this._saldo;
-	}
-
-	constructor(agencia, cliente ) {  // construtor adicionado para inicializar os atributos agencia e cliente
-		this.agencia = agencia;
-		this.cliente = cliente;
-		ContaCorrente.numeroDeContas += 1;
 	}
 
 	// criando uma função para saque
