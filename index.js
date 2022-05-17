caa91ac854234015e746b51c124ed508b763c8b6
Uziel Menezes <uzielcivil@gmail.com>
2022-05-17 06:48:42 -0300

    Creating the object ContaPoupanca with its properties

--- index.js
+++ index.js
@@ -3,19 +3,15 @@
 
 import {Cliente} from "./Cliente.js" // importando os módulos necessários para o projeto
 import {ContaCorrente} from "./ContaCorrente.js"
+import {ContaPoupanca} from "./ContaPoupanca.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309); // objetos criados a partir do molde Cliente com constructor definido
-const cliente2 = new Cliente("Alice", 88822233309);
-
-console.log(cliente1);
-console.log(cliente2);
 
 const contaCorrente1 = new ContaCorrente(1001, cliente1); // objetos/instâncias criados a partir do molde ContaCorrente com constructor definido;
 contaCorrente1.depositar(500);
+contaCorrente1.sacar(100);
 
-const contaCorrente2 = new ContaCorrente(1001, cliente2);
-contaCorrente1.transferir(200, contaCorrente2);
+const contaPoupanca1 = new ContaPoupanca(50, cliente1, 1001);
 
+console.log(contaPoupanca1);
 console.log(contaCorrente1);
-console.log(contaCorrente2);
-console.log(ContaCorrente.numeroDeContas);
